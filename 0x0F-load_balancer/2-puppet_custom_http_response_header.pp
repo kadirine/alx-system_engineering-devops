633782eb4c3f266c74a493c3b2ea40c41221911a
kadirine <mohamedmo3iz99@gmail.com>
2024-03-05 16:19:25 -0800

    correct tasks 0x0F. Load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+# Puppet Manifest to configure Nginx with custom HTTP response header on a new Ubuntu machine.
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
